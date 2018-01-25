15f5db45571bceae8dab780c26f680ca4eb4815d
s00165427 <s00165427@mail.itsligo.ie>
2018-01-25 10:38:50 +0000

    added email field migration

--- Exercise12/Models/User.cs
+++ Exercise12/Models/User.cs
@@ -11,5 +11,6 @@ namespace Exercise12.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DOB { get; set; }
+        public string Email { get; set; }
     }
 }
