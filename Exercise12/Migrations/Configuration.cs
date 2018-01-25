76c1fe90236eeb7ea7da994629c8a9f7ef0aa9a8
s00165427 <s00165427@mail.itsligo.ie>
2018-01-25 10:30:16 +0000

    add first migration

---
+++ Exercise12/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace Exercise12.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Exercise12.Models.Exercise12Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "Exercise12.Model.Exercise12Context";
+        }
+
+        protected override void Seed(Exercise12.Models.Exercise12Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
